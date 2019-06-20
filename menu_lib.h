6f48f3e0549f5043025d2755a8ca323e084e70de
eyGENIUS <51393371+eyGENIUS@users.noreply.github.com>
2019-06-20 06:34:53 +0300

    Create menu_lib.h

---
+++ menu_lib.h
@@ -0,0 +1,16 @@
+#ifndef MENU_LIB_H_INCLUDED
+#define MENU_LIB_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+void menu(List *list);///меню
+
+void menuSearch(List *list);///меню поиска
+
+void menuAdd(List *list);///меню добавления
+
+void menuDelete(List *list);///меню удаления
+
+void menuEdit(List *list);///меню редактирования
+
+#endif // MENU_LIB_H_INCLUDED
