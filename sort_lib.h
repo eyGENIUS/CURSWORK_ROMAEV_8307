623001fe6005b8bbf12e20f4f29061fe5fe0edcd
eyGENIUS <51393371+eyGENIUS@users.noreply.github.com>
2019-06-20 06:36:21 +0300

    Create sort_lib.h

---
+++ sort_lib.h
@@ -0,0 +1,21 @@
+#ifndef SORT_LIB_H_INCLUDED
+#define SORT_LIB_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap(PI *a, PI *b);///меняем пестами
+
+void sortId(List *list);///сортировки
+
+void sortYear(List *list);
+
+void sortFuel(List *list);
+
+void sortName(List *list);
+
+void sortType(List *list);
+
+void sortPrice(List *list);
+
+
+#endif // SORT_LIB_H_INCLUDED
