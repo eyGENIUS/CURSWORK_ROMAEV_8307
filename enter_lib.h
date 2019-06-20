f3be9d2c5282bd2b85a04a6fbb32e665e9f556fa
eyGENIUS <51393371+eyGENIUS@users.noreply.github.com>
2019-06-20 06:31:36 +0300

    Create enter_lib.h

---
+++ enter_lib.h
@@ -0,0 +1,10 @@
+#ifndef ENTER_LIB_H_INCLUDED
+#define ENTER_LIB_H_INCLUDED
+#include <stdio.h>
+#include <stdlib.h>
+
+Int(char *massage);
+
+String(char *massage);
+
+#endif // ENTER_LIB_H_INCLUDED
